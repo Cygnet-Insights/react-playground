f3c340d2b6fff4743bd8f6b8f27ddfb9a8c8803f
Thoth Ky <28805113+thoth-ky@users.noreply.github.com>
2020-04-29 09:30:43 +0300

    Table update

--- src/App.js
+++ src/App.js
@@ -1,24 +1,37 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {useState } from 'react';
 import './App.css';
 
+const initialStuff =[[1,"a","b"], [2,"c","d"], [3,"e","f"]]
+
 function App() {
+
+  const [stuff, setStuff] = useState(initialStuff)
+
+  const updateStuff = () => {
+    const newStuff = [[1,"a","b"], [2,"c","d"], [3,"new","new"]]
+    setStuff(newStuff)
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <table>
+        <tr>
+          <th>ID</th>
+          <th>First Column</th>
+          <th>Second Column</th>
+        </tr>
+        {
+        stuff.map(item => {
+          return (
+            <tr key={item[0]}>
+              <td>{item[0]}</td>
+              <td>{item[1]}</td>
+              <td>{item[2]}</td>  
+            </tr>
+          )
+        })
+        }
+      </table>
+      <button onClick={updateStuff}>Update Table</button>
     </div>
   );
 }
